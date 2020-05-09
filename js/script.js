66534cd98e02312a7cea697392b53fa6ddc668b4
mrony338 <md_rony@comcast.com>
2020-05-08 23:03:05 -0400

    home Page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+$("#hireMeButton").click(function () {
+    $("#hireMeModal").modal("show");
+});
